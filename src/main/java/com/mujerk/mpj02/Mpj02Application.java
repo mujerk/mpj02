864ca6446fb6b1d351662668b563a25b605f3216
bsh <mujerk@gmail.com>
2021-04-01 17:59:33 +0900

    init

---
+++ src/main/java/com/mujerk/mpj02/Mpj02Application.java
@@ -0,0 +1,13 @@
+package com.mujerk.mpj02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mpj02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mpj02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mujerk/mpj02/Mpj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mujerk.mpj02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mpj02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
